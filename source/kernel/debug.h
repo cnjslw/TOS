0444ecd3097f9123425f4333141f559e1a882da6
root <root@DESKTOP-5JLOCH7.localdomain>
2023-05-02 00:19:32 +0800

    Debugs:assert invaild

--- source/kernel/debug.h
+++ source/kernel/debug.h
@@ -1,19 +1,19 @@
 #ifndef __KERNEL_DEBUG_H
 #define __KERNEL_DEBUG_H
 
-void panic_spin(char* filename, int line, const char* func, const char* conditon);
+void panic_spin(char* filename, int line, const char* func, const char* condition);
 
-#define PANIC(...) panic_spin(__FILE__, __LINE__, __func__, __VA_ARGS__);
+#define PANIC(...) panic_spin(__FILE__, __LINE__, __func__, __VA_ARGS__)
 
-#ifndef NDEBUG
-#define ASSERT(condition) ((void)0)
+#ifdef NDEBUG
+#define ASSERT(CONDITION) ((void)0)
 #else
-#define ASSERT(CONDITION)  \
-    if (CONDITION) {       \
-    } else {               \
-        PANIC(#CONDITION); \
+#define ASSERT(CONDITION)                                                   \
+    if (CONDITION) {                                                        \
+    } else {                                                                \
+        /* 符号#让编译器将宏的参数转化为字符串字面量 */ \
+        PANIC(#CONDITION);                                                  \
     }
+#endif /*__NDEBUG */
 
-#endif
-
-#endif
+#endif /*__KERNEL_DEBUG_H*/
