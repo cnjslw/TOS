030026a94f8ff6f9f4a6744e035174ff84d8b229
LW <cnjslw@foxmail.com>
2023-09-04 01:31:58 +0800

    Load kernel and hardware info

---
+++ source/kernel/init/init.c
@@ -0,0 +1,25 @@
+﻿/**
+ * 内核初始化以及测试代码
+ *
+ *
+ */
+#include "comm/boot_info.h"
+
+int global_var = 0x1234;
+int globa_var_zero;
+static int static_global_var = 0x2345;
+static int static_global_var_zero;
+
+const int const_int = 0x33;
+const char * str = "abcdefg";
+
+/**
+ * 内核入口
+ */
+void kernel_init (boot_info_t * boot_info) {
+    int locar_var;
+    static int static_local_var = 0x33;
+    static int static_local_var_zero;
+
+    for (;;) {}
+}
